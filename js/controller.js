1ec7e14e71f858c12d8f5c971e2a1283efaf2c1a
nendhruv <nendhruv@yahoo.com>
2015-05-09 18:51:06 +0530

    simple todo-list

---
+++ js/controller.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/* Controllers */
+
+var ListApp = angular.module('ListApp', []);
+
+ListApp.controller('ListController', function($scope) {
+
+
+  var itemName = [];
+
+var list = JSON.parse(localStorage.getItem("item"));
+$scope.lists= list;
+  $scope.saveItem = function(){
+  	var name= $scope.item.name;
+    var list = JSON.parse(localStorage.getItem("item"));
+  	console.log(list);
+    if(list==null){
+  	itemName.push(name);
+  	localStorage.setItem("item", JSON.stringify(itemName));
+    $scope.lists= itemName;
+    }
+    else{
+       list.push(name);
+       localStorage.setItem("item",JSON.stringify(list));
+       $scope.lists= list;
+    }
+  }
+
+  	$scope.remove = function(index){
+  		 var list = JSON.parse(localStorage.getItem("item"));
+  		 console.log(list);
+  		 list.splice(index,1);
+  		 console.log(list);
+  		 localStorage.setItem("item",JSON.stringify(list));
+  		 $scope.lists= list;
+
+  	}
+
+    $scope.editmode = false;
+
+  	 $scope.toggleEditMode = function(){
+    $scope.editmode = $scope.editmode === true ? false: true;
+  }
+
+  $scope.edit = function(value, index){
+     var list = JSON.parse(localStorage.getItem("item"));
+     list[index]=value;
+       localStorage.setItem("item",JSON.stringify(list));
+       $scope.lists= list;
+       $scope.editmode= false;
+  }
+
+});
+
+
+/** directive **/
+
+ListApp.directive("contenteditable", function() {
+  return {
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
+
+      function read() {
+        ngModel.$setViewValue(element.html());
+      }
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+      element.bind("blur keyup change", function() {
+        scope.$apply(read);
+      });
+    }
+  };
+});
